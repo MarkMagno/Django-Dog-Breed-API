f0716d394cfb9daf21ccc1e4dac24eee3dc7d693
MarkMagno <44620752+MarkMagno@users.noreply.github.com>
2024-10-14 17:56:27 -0500

    Update views.py

--- dog_project/dogapi/views.py
+++ dog_project/dogapi/views.py
@@ -1,1 +1,12 @@
+from rest_framework import viewsets
+from .models import Dog, Breed
+from .serializers import DogSerializer
+from .serializers import BreedSerializer
 
+class DogViewSet(viewsets.ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+class BreedViewSet(viewsets.ModelViewSet):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
